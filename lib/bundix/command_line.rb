b8f45b9dbdbd2515d4fd201f3d6a495a57af6062
Burke Libbey <burke@libbey.me>
2020-02-24 22:43:52 -0500

    stuff

--- lib/bundix/command_line.rb
+++ lib/bundix/command_line.rb
@@ -1,6 +1,7 @@
 # typed: strong
 # frozen_string_literal: true
 require('bundix')
+require('net/http')
 require('optparse')
 require('tempfile')
 
@@ -9,6 +10,63 @@ module Bundix
   # given a Gemfile.lock.
   class CommandLine
     extend(T::Sig)
+
+    class SHAs
+      def self.run
+        new.run
+      end
+
+      def best(have, want)
+        r = have.detect { |h| want =~ h }
+        return r if r
+        return nil if want.version.nil? || want.version == "13"
+        want.version = (want.version.to_i - 1).to_s
+        best(have, want)
+      end
+
+      def run
+        lockfile = STDIN.read
+
+        lp = ::Bundix::LockfileToGemset.send(:parse_lockfile, lockfile)
+
+        lp.specs.each do |spec|
+          data = {
+            name: spec.name,
+            full_name: spec.full_name,
+            version: spec.version.to_s,
+            platform: spec.platform.to_s,
+          }
+          case (source = spec.source)
+          when ::Bundler::Source::Rubygems
+            remotes = source.remotes.map { |remote| remote.to_s.sub(%r{/+$}, '') }
+            remotes.each do |remote|
+              if remote == "https://rubygems.org"
+                Net::HTTP.start('rubygems.org', 443, use_ssl: true) do |http|
+                  resp = http.request(Net::HTTP::Get.new("/api/v1/versions/#{spec.name}.json"))
+                  jd = JSON.parse(resp.body)
+                  datas = jd.select { |d| d['number'] == spec.version.to_s }
+                  if datas.size == 1
+                    puts datas.first.values_at('sha', 'platform')
+                  else
+                    have = datas.map { |f| f['platform'] }
+                    want = Gem::Platform.local
+                    plat = best(have, want)
+                    puts datas.detect { |f| f['platform'] == plat.to_s }.values_at('sha', 'platform')
+                  end
+                end
+              else
+              end
+            end
+
+            # puts(::Bundix::Nixer.serialize(data.merge(remotes: remotes)))
+          when ::Bundler::Source::Git
+            puts 'git'
+          when ::Bundler::Source::Path
+            puts 'path'
+          end
+        end
+      end
+    end
 
     sig { void }
     def self.run
